a802cd3c4a27beb92f1debc83af9fd559249828c
chiefkat05 <jamesville05@gmail.com>
2024-04-29 14:55:51 -0500

    asynchronous server time!

--- multiplayer.h
+++ multiplayer.h
@@ -8,22 +8,84 @@
 #include <ctime>
 
 using asio::ip::tcp;
+class tcp_connection
+    : public std::enable_shared_from_this<tcp_connection>
+{
+public:
+    typedef std::shared_ptr<tcp_connection> pointer;
+
+    static pointer create(asio::io_context &io_context)
+    {
+        return pointer(new tcp_connection(io_context));
+    }
+
+    tcp::socket &socket()
+    {
+        return socket_;
+    }
+
+    void start()
+    {
+        message_ = "connection message pending\n";
+
+        asio::async_write(socket_, asio::buffer(message_),
+                          std::bind(&tcp_connection::handle_write, shared_from_this(),
+                                    asio::placeholders::error, asio::placeholders::bytes_transferred));
+    }
+
+private:
+    tcp_connection(asio::io_context &io_context)
+        : socket_(io_context) {}
+
+    void handle_write(const std::error_code & /*error*/, size_t /*bytes_transferred*/) {}
+
+    tcp::socket socket_;
+    std::string message_;
+};
+
+class tcp_server
+{
+public:
+    tcp_server(asio::io_context &io_context)
+        : io_context_(io_context),
+          acceptor_(io_context, tcp::endpoint(tcp::v4(), 4444))
+    {
+        start_accept();
+    }
+
+private:
+    void start_accept()
+    {
+        tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
+
+        acceptor_.async_accept(new_connection->socket(),
+                               std::bind(&tcp_server::handle_accept,
+                                         this, new_connection, asio::placeholders::error));
+    }
+    void handle_accept(tcp_connection::pointer new_connection,
+                       const std::error_code &error)
+    {
+        if (!error)
+        {
+            new_connection->start();
+        }
+
+        start_accept();
+    }
+
+    asio::io_context io_context_;
+    tcp::acceptor acceptor_;
+};
+
 void server()
 {
     try
     {
         asio::io_context io_context;
 
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 4444));
+        tcp_server server(io_context);
 
-        for (;;)
-        {
-            tcp::socket socket(io_context);
-            acceptor.accept(socket);
-
-            std::error_code ignored_error;
-            asio::write(socket, asio::buffer("server hears you!"), ignored_error);
-        }
+        io_context.run();
     }
     catch (const std::exception &e)
     {
